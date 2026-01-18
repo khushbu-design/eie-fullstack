91bbf1c45d72c38181bbfbabdc2ecee1f9ea8f25
khushbu-design <khushbu@eieinstruments.com>
2026-01-18 15:51:37 +0530

    Revert plugins.ts to empty (remove transfer config)

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,13 +1,1 @@
-export default ({ env }) => ({
-  transfer: {
-    enabled: true,
-    config: {
-      token: {
-        salt: env('TRANSFER_TOKEN_SALT', 'a-very-long-random-salt-for-transfer-2026'), // .envમાં પણ આ જ salt નાખો
-      },
-      remote: {
-        enabled: true,
-      },
-    },
-  },
-});
+export default () => ({});
