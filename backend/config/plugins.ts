4ace19d66fe405cfe45c6fa304fb2a9798de6e9e
khushbu-design <khushbu@eieinstruments.com>
2026-01-18 15:47:36 +0530

    Enable remote data transfer in plugins.ts for Strapi Cloud

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default ({ env }) => ({
+  transfer: {
+    enabled: true,
+    config: {
+      token: {
+        salt: env('TRANSFER_TOKEN_SALT', 'a-very-long-random-salt-for-transfer-2026'), // .envમાં પણ આ જ salt નાખો
+      },
+      remote: {
+        enabled: true,
+      },
+    },
+  },
+});
